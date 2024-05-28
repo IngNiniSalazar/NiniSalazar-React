34d0c1d95b2bde050982a70e4eb8a7203af04ef9
IngNiniSalazar <johannaquitian140@gmail.com>
2024-05-27 23:59:38 -0500

    trabajo 4 modulo React

--- ProyectoReact/proyectCinemaCucuta/src/App.jsx
+++ ProyectoReact/proyectCinemaCucuta/src/App.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import Card from './components/Cards';
+import { useState, useEffect } from 'react'//guardar una variable
+import Card from './components/Cards'
 import './App.css'
 
 
 function App() {
-  // Recuerda que este es solo un componente funcional, 
-  // tu debes incluir estilos para hacerlo más atractivo
+  const [pagina, setPagina] = useState(0) //eje central de la aplicacion
+  const [personajes, setPersonajes] = useState([])//arreglo vacio
+  useEffect(() => {
+    fetch(`https://rickandmortyapi.com/api/character/?page=${pagina}`)//personajes de rickimortin
+      .then((response) => response.json())
+      .then((data)=>setPersonajes(data.results))//informacion de los personajes se guarde
+  }, [pagina])
+  console.log(pagina)
+  const incrementarContador = () => {
+    setPagina(pagina + 1)
+  }
+  const decrementaContador = () => {
+    setPagina(pagina - 1)
+  }
+
   return (
     <div>
-      <Card title="Garfield" />
-
+      {personajes.length !==0 && personajes.map((personaje)=>(//repite el codigo cuantas veces hay el personaje
+        <Card key ={personaje.id} title={personaje.name} genre={personaje.gender} status={personaje.status} img={personaje.image}/>
+      ))}
+      <button classname="efecto" onClick={decrementaContador}>-Contador</button>
+      <button classname="efecto" onClick={incrementarContador}>+Contador</button>
+      
     </div>
   );
 }
--- ProyectoReact/proyectCinemaCucuta/src/components/Cards.jsx
+++ ProyectoReact/proyectCinemaCucuta/src/components/Cards.jsx
@@ -1,18 +1,17 @@
 
 import Title from "./titulo"
 import Image from "./imagen"
-import GatoAnimado from "../assets/garfield.jpg"
 import Details from "./details"
 
 
 const Card = (props) => {
-    return(
-    <div className="container">
-        
-        <Image url={GatoAnimado}/>
-        <Title title={props.title}/>
-        <Details genre="Infantil" status ="Activo de 2:00 a 4:00"/>
-    </div>
+    return (
+            <div className="container">
+
+                <Image url={props.img} />
+                <Title title={props.title} />
+                <Details genre={props.genre} status={props.status} />
+            </div>
     )
 }
 
